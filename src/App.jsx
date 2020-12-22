c9aeec48f799f76fddc509a160398a6e06b331fd
Neto Lourenco <otaviojnetoo@gmail.com>
2020-12-22 15:00:11 -0300

    Configurando a estrutura do Sprivaten

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
 import React from 'react';
+import Routes from './routes'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Em construção!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes />
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Home from './pages/index';
 
 ReactDOM.render(
   <React.StrictMode>
