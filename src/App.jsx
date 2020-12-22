6a543a5fb0672f63a08254c5b38f7cd0b3fdb9f8
Neto Lourenco <otaviojnetoo@gmail.com>
2020-12-22 14:18:15 -0300

    iniciando sprivaten

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Em construção!
         </p>
         <a
           className="App-link"
@@ -15,7 +13,6 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
